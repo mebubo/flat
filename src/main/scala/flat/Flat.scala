d56ee1cb4e811f7992f3caea1e712947331d94d6
Sergei Dolgov <dolgovs@gmail.com>
2018-09-10 23:17:22 +0200

    Use LabelledGeneric

--- src/main/scala/flat/Flat.scala
+++ src/main/scala/flat/Flat.scala
@@ -3,6 +3,7 @@ package flat
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import shapeless._
+import shapeless.labelled.FieldType
 
 trait Flat[A] {
   def row(a: A): Row
@@ -26,9 +27,9 @@ object Flat {
   implicit def flatOption[A](implicit fa: Flat[A]): Flat[Option[A]] = new Flat[Option[A]] {
     override def row(a: Option[A]): Row = a.fold(Row(Seq.fill(fa.schema.fields.length)(null): _*))(a => fa.row(a))
     override def schema: StructType = {
-        val s = fa.schema
-        s.copy(fields = s.fields.map(_.copy(nullable = true)))
-      }
+      val s = fa.schema
+      s.copy(fields = s.fields.map(_.copy(nullable = true)))
+    }
   }
 
   implicit val flatHNil: Flat[HNil] = new Flat[HNil] {
@@ -36,18 +37,21 @@ object Flat {
     override def schema: StructType = StructType(Seq())
   }
 
-  implicit def flatHList[H, T <: HList](implicit hi: Lazy[Flat[H]], ti: Flat[T]): Flat[H :: T] = new Flat[H :: T] {
-    override def row(a: H :: T): Row = a match {
-      case h :: t => Row.merge(hi.value.row(h), ti.row(t))
-    }
-    override def schema: StructType = {
-      val h: Array[StructField] = hi.value.schema.fields
-      val t: Array[StructField] = ti.schema.fields
-      StructType(h ++ t)
-    }
+  implicit def flatHList[K <: Symbol, H, T <: HList](implicit witness: Witness.Aux[K], hi: Lazy[Flat[H]], ti: Flat[T]): Flat[FieldType[K, H] :: T] =
+    new Flat[FieldType[K, H] :: T] {
+      override def row(a: FieldType[K, H] :: T): Row = a match {
+        case h :: t => Row.merge(hi.value.row(h), ti.row(t))
+      }
+
+      override def schema: StructType = {
+        val fieldName: String = witness.value.name
+        val h: Array[StructField] = hi.value.schema.fields.map(f => f.copy(name = s"$fieldName.${f.name}"))
+        val t: Array[StructField] = ti.schema.fields
+        StructType(h ++ t)
+      }
   }
 
-  implicit def flatGeneric[A, R] ( implicit generic: Generic.Aux[A, R], flat: Lazy[Flat[R]] ): Flat[A] = new Flat[A] {
+  implicit def flatGeneric[A, R] ( implicit generic: LabelledGeneric.Aux[A, R], flat: Lazy[Flat[R]] ): Flat[A] = new Flat[A] {
     override def row(a: A): Row = flat.value.row(generic.to(a))
     override def schema: StructType = flat.value.schema
   }
